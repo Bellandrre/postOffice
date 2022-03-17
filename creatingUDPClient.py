d5f536ec3c5e6c4382266ac8a2676cab06c141f4
Karthik Kolathumani <32917045+KarthikKola29@users.noreply.github.com>
2022-03-17 07:18:41 -0700

    Update creatingUDPClient.py

--- creatingUDPClient.py
+++ creatingUDPClient.py
@@ -1,15 +1,24 @@
 import socket
 
 MAX_SIZE_BYTES = 65535
+HOST = '127.0.0.1'
+PORT = '3000'
 
 def client():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+
+    s.connect((HOST, PORT))
+
     inputMessage = input('Input lowercase sentence: ')
     data = inputMessage.encode('ascii')
-    s.sendto(data, ('127.0.0.1', 3000))
+
     logMessage = 'The OS assigned the ephemeral port {} to me'.format(s.getsockname())
     print(logMessage)
-    data, address = s.recvfrom(MAX_SIZE_BYTES) ## Receive from the server
+
+    s.send(data)
+
+    data, address = s.recv(MAX_SIZE_BYTES) ## Receive from the server
     serverText = data.decode('ascii')
+
     print('The server {} replied with {!r}'.format(address, serverText))
 
